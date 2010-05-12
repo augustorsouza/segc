0600cb1fed05df87b8dd7d2c0fc0bdb5d11936a7
Augusto Souza <augustorsouza@gmail.com>
2010-05-12 20:46:27 -0300

    Versão que trata os argumentos de entrada

--- trabalho06/dns-spoof.c
+++ trabalho06/dns-spoof.c
@@ -4,13 +4,12 @@ Autores:
     Leonardo Maia Barbosa - ra107213 - leomaia_eco@yahoo.com.br
 
 Arquivo: 
-    extractor.c
+    dns-spoof.c
 
 parametros:
-    --victim_ip  = IP do host monitorado   
-    --victim-ethernet = MAC do host monitorado
-    -proto Protocolos de aplicação que serão monitorados
-    <arquivo pcap> Arquivo pcap do trafego a ser monitorado
+    --interface   = A interface que será ouvida, ex.: eth0   
+    --requisition = Dominio o qual o programa irá responder caso haja uma requisição DNS, ex.: www.uol.com.br
+    --ip          = O ip da maquina que será informado na resposta na requisição DNS, ex.: 10.0.0.1
            
 plataforma:
     linux
@@ -21,185 +20,48 @@ plataforma:
 #include <stdio.h>
 #include <string.h>
 #include <pcap.h>
-#include <netdb.h>
 
 /* Constantes: */
 #define MAX_SIZE 256
-#define PARAMETERS_NUMBER 8
-#define MAX_PROTOCOLS_NUMBER 128
-
-#define ONLY_TCP_FILTER "tcp && host " // filtro para contar pacotes tcp
-#define ONLY_UDP_FILTER "udp && host " // filtro para contar pacotes udp
-#define TCP_SESSIONS_FILTER "(tcp[tcpflags] & tcp-syn) != 0 && (tcp[tcpflags] & tcp-ack) == 0 && host " // filtro para contar sessões tcp
-#define APPLICATION_PROTOCOL_SESSIONS_FILTER "(tcp[tcpflags] & tcp-syn) != 0 && (tcp[tcpflags] & tcp-ack) == 0 && port " // filtro para contar sessões tcp de um determinado protocolo de aplicação
-
-/* Numeradores */
-enum FILTER_TYPE { 
-    ONLY_TCP, 
-    ONLY_UDP,
-    TCP_SESSIONS,
-    APPLICATION_PROTOCOL_SESSIONS
-};
+#define PARAMETERS_NUMBER 7
 
 /* Variáveis globais: */
-char victim_ip[MAX_SIZE] = "";
-char victim_ethernet[MAX_SIZE] = "";
-char proto[MAX_SIZE] = "";
-char pcap_filename[MAX_SIZE] = "";
-pcap_t *pcap_descriptor = NULL;
-char protocols[MAX_PROTOCOLS_NUMBER][MAX_SIZE];
-int protocols_quantity;
-
-/* Função para contar o número de pacotes de acordo com um determinado filtro */
-int packet_count(enum FILTER_TYPE filter, char *protocol) {
-    struct bpf_program fp;		    // filtro compilado
-    struct pcap_pkthdr *pkt_header; // cabeçalho de um determinado pacote, informação que não utilizaremos
-    const u_char *pkt_data;         // dados de um determinado pacote, informação que não utilizaremos
-    char filter_exp[MAX_SIZE] = "";	// expressão para descrever o filtro
-    int count = 0;                  // contador de pacotes
-
-    switch (filter) {
-        case ONLY_TCP:
-            strcat(filter_exp, ONLY_TCP_FILTER);
-            strcat(filter_exp, victim_ip);
-        break;
-        case ONLY_UDP:
-            strcat(filter_exp, ONLY_UDP_FILTER);
-            strcat(filter_exp, victim_ip);
-        break;
-        case TCP_SESSIONS:
-            strcat(filter_exp, TCP_SESSIONS_FILTER);
-            strcat(filter_exp, victim_ip);
-        break;
-        case APPLICATION_PROTOCOL_SESSIONS:
-            strcat(filter_exp, APPLICATION_PROTOCOL_SESSIONS_FILTER);
-            strcat(filter_exp, protocol);
-            strcat(filter_exp, "&& host ");
-            strcat(filter_exp, victim_ip);
-        break;
-    }
-
-	if (pcap_compile(pcap_descriptor, &fp, filter_exp, 0, 0) == -1) {
-		fprintf(stderr, "Não foi possível interpretar o filtro %s: %s\n", filter_exp, pcap_geterr(pcap_descriptor));
-		exit(8);
-	}
-	
-	if (pcap_setfilter(pcap_descriptor, &fp) == -1) {
-		fprintf(stderr, "Não foi possível interpretar o filtro %s: %s\n", filter_exp, pcap_geterr(pcap_descriptor));
-		exit(8);
-	}
-	
-	/* Após aplicado o filtro basta contar a quantidade de pacotes no descriptor */
-	while (pcap_next_ex(pcap_descriptor, &pkt_header, &pkt_data) == 1) 
-	    count++;
-   
-    return count;
-}
-
-/* Função para abrir (caso ainda não tenha sido aberto) ou reabrir arquivo pcap */
-void open_or_reload_pcap_file() {
-    char *ebuf;
-    
-    /* Se o arquivo já estiver aberto, então o fecharemos */
-    if (pcap_descriptor != NULL)
-        pcap_close(pcap_descriptor);
-
-    /* Abre o arquivo pcap_filename com checagem de erros */        
-    if ((pcap_descriptor = pcap_open_offline(pcap_filename, ebuf)) == NULL) {
-        printf("ERRO NA REABERTURA DO ARQUIVO PCAP %s: %s\n", pcap_filename, ebuf);
-        exit(8);
-    }
-}
-
-/* Função auxiliar para a função do quicksort do C. Utilizada para ordenar os protocolos segundo o seus numeros de porta. */
-int compare_port_numbers(x, y)
-void *x, *y; 
-{
-    int port_x, port_y;
-    
-    port_x = ntohs((*getservbyname((char*)x, "tcp")).s_port);
-    port_y = ntohs((*getservbyname((char*)y, "tcp")).s_port);
-
-    if (port_x > port_y)
-        return 1;
-    else if (port_x == port_y)
-        return 0;
-    else if (port_x < port_y)
-        return -1;
-}
+char interface[MAX_SIZE] = "";
+char requisition[MAX_SIZE] = "";
+char ip[MAX_SIZE] = "";
 
 /* Função para mostrar padrao de execucao */
 void show_usage_and_exit(void)
 {
 	printf("Padrao de utilizacao:\n");
-	printf("[obrigatorio] --victim_ip  IP do host monitorado\n");
-	printf("[obrigatorio] --victim_ethernet MAC do host monitorado\n");
-	printf("[obrigatorio] -proto Protocolos de aplicação que serão monitorados\n");
-	printf("[obrigatorio] Passar arquivo PCAP como ultimo parametro\n");
-	printf("Exemplo de utilização: ./extractor --victim-ip 10.0.0.1 --victim-ethernet 00:0f:20:2f:63:d9 -proto http,ftp,smtp teste.pcap\n");
+   	printf("[obrigatorio] --interface   = A interface que será ouvida, ex.: eth0 \n");
+	printf("[obrigatorio] --requisition = Dominio o qual o programa irá responder caso haja uma requisição DNS, ex.: www.uol.com.br\n");
+	printf("[obrigatorio] --ip          = O ip da maquina que será informado na resposta na requisição DNS, ex.: 10.0.0.1\n");
+	printf("Exemplo de utilização: dns-spoof --interface eth0 --requisition www.uol.com.br --ip 10.0.0.1\n");
 	exit(8);
 }
 
 /* Função main */
 int main(int argc, char *argv[]) {
-    int i=0; // contador auxiliar
-    
+    int i; // contador auxiliar
     /* Se o numero de paramestros passados não é o esperado, exibe mensagem de erro */    
     if (argc != PARAMETERS_NUMBER) 
         show_usage_and_exit();
     
     /* Extrai parametros */
-    for(i=1; i<argc; i=i+2) {
-        if (!strcmp(argv[i], "--victim-ip"))
-            strcpy(victim_ip, argv[i+1]);
-        else if (!strcmp(argv[i], "--victim-ethernet"))
-            strcpy(victim_ethernet, argv[i+1]);
-        else if (!strcmp(argv[i], "-proto"))
-            strcpy(proto, argv[i+1]);
+    for(i=1; i<PARAMETERS_NUMBER; i=i+2) {
+        if (!strcmp(argv[i], "--interface"))
+            strcpy(interface, argv[i+1]);
+        else if (!strcmp(argv[i], "--requisition"))
+            strcpy(requisition, argv[i+1]);
+        else if (!strcmp(argv[i], "--ip"))
+            strcpy(ip, argv[i+1]);
     }
-    
-    /* O ultimo parametro passado na linha de comando é o arquivo pcap */
-    strcpy(pcap_filename, argv[PARAMETERS_NUMBER-1]);
 
     /* Se algum dos parametros não foi settado corretamente, exibe mensagem de erro */
-    if ((!strcmp(victim_ip, "")) || (!strcmp(victim_ethernet, "")) || (!strcmp(proto, "")) || (!strcmp(pcap_filename, "")))
+    if ((!strcmp(interface, "")) || (!strcmp(requisition, "")) || (!strcmp(ip, "")))
         show_usage_and_exit();
-    
-    /* Imprime quantidade de pacotes tcp */
-    open_or_reload_pcap_file();
-    printf("%d\n", packet_count(ONLY_TCP, NULL));
 
-    /* Imprime quantidade de pacotes udp */    
-    open_or_reload_pcap_file();
-    printf("%d\n", packet_count(ONLY_UDP, NULL));
-
-    /* Imprime quantidade de pacotes sessões tcp */        
-    open_or_reload_pcap_file();
-    printf("%d\n", packet_count(TCP_SESSIONS, NULL));
-
-    /* Monta array de string com os protocolos de aplicação e os ordena pelo numero da porta */
-    char delims[] = ",";
-    char *result = NULL;
-    result = strtok(proto, delims);
-    i = 0;
-    while( result != NULL ) {
-        strcpy(protocols[i], result);
-        i++;
-        result = strtok(NULL, delims);
-    }
-
-    protocols_quantity = i;
-
-    qsort(protocols, (size_t)protocols_quantity, sizeof(char)*MAX_SIZE, compare_port_numbers);
-
-    /* Imprime para cada protocolo de aplicação passado como parametro a quantidade de sessões tcp */    
-    for (i=0; i<protocols_quantity; i++) {
-        open_or_reload_pcap_file();
-        printf("%d\n", packet_count(APPLICATION_PROTOCOL_SESSIONS, (char*)protocols[i]));
-    }
-    
-    /* Feche o arquivo e termine a execução */
-    pcap_close(pcap_descriptor);
     return(0);
 }
 
